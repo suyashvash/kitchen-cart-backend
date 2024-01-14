b2fa8a63c40bcb44eb933032707f6276c1647a77
Suyash Vashishtha <suyashvashishtha@gmail.com>
2024-01-15 00:05:52 +0530

    Added similar search

--- routes/product.js
+++ routes/product.js
@@ -32,12 +32,12 @@ productRouter.route('/add').post((req, res) => {
         newProduct.save()
             .then(() => sendResponse(res, true, newProduct, "Product added !", 200))
             .catch(err => sendResponse(res, false, err, "Product not added !", 400));
-    }else{
+    } else {
         sendResponse(res, false, null, "You are not authorized to add product !", 401);
     }
 })
 
-productRouter.route('/:id').get((req, res) => {
+productRouter.route('/findById/:id').get((req, res) => {
     Product.findById(req.params.id)
         .then(product => sendResponse(res, true, product, "Product found !", 200))
         .catch(err => sendResponse(res, false, err, "Product not found !", 400));
@@ -50,9 +50,59 @@ productRouter.route('/:id').delete((req, res) => {
         Product.findByIdAndDelete(req.params.id)
             .then(() => sendResponse(res, true, null, "Product deleted !", 200))
             .catch(err => sendResponse(res, false, err, "Product not deleted !", 400));
-    }else{
+    } else {
         sendResponse(res, false, null, "You are not authorized to delete product !", 401);
     }
+})
+
+productRouter.route('/similar').get((req, res) => {
+
+    const category = req.query.category;
+
+    if (!category) { 
+        sendResponse(res, false, null, "Category not found !", 400); 
+        return;
+    }
+
+
+    Product.find({ category: category })
+        .then(products => sendResponse(res, true, products, "Products found !", 200))
+        .catch(err => sendResponse(res, false, err, "Products not found !", 400));
+
+})
+
+productRouter.route('/byBrand').get((req, res) => {
+
+    const category = req.query.text;
+
+    if (!category) { 
+        sendResponse(res, false, null, "Brand not found !", 400); 
+        return;
+    }
+
+  
+
+
+    Product.find({ brand: category })
+        .then(products => sendResponse(res, true, products, "Products found !", 200))
+        .catch(err => sendResponse(res, false, err, "Products not found !", 400));
+
+})
+
+// Search product
+productRouter.route('/search').get((req, res) => {
+
+    const search = req.query.query;
+
+    if (!search) {
+        sendResponse(res, false, null, "Search not found !", 400);
+        return;
+    }
+
+    Product.find({ title: { $regex: search, $options: 'i' }})
+        .then(products => sendResponse(res, true, products, "Products found !", 200))
+        .catch(err => sendResponse(res, false, err, "Products not found !", 400));
+
 })
 
 
@@ -75,7 +125,7 @@ productRouter.route('/update/:id').post((req, res) => {
                     .catch(err => sendResponse(res, false, err, "Product not updated !", 400));
             })
             .catch(err => sendResponse(res, false, err, "Product not found !", 400));
-    }else{
+    } else {
         sendResponse(res, false, null, "You are not authorized to update product !", 401);
     }
 })
