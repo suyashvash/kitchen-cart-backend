b1d16d9a1e9efcfcd07bd59d8ef828ade03205d0
Suyash Vashishtha <suyashvashishtha@gmail.com>
2024-01-21 00:10:59 +0530

    Added check for add cart

--- routes/cart.js
+++ routes/cart.js
@@ -15,7 +15,7 @@ cartRouter.route('/').get((req, res) => {
         .then(user => {
             if (user) {
                 let cart = user.cart;
-                if(user.cart.length==0){
+                if (user.cart.length == 0) {
                     sendResponse(res, true, [], "Cart found !", 200)
                     return
                 }
@@ -53,34 +53,50 @@ cartRouter.route("/add").post((req, res) => {
     const userId = req.get('token');
     const { productId, quantity } = req.body;
 
+    User.findOne({ _id: userId })
+        .then(user => {
+            if (user) {
 
-    Product.findOne({ _id: productId })
-        .then(product => {
-            if (product) {
 
-                const newCartItem = new CartItem({
-                    productId,
-                    userId,
-                    quantity,
-                })
+                if(user.cart.length > 0){
+                    let thisItem = user.cart.find(item => item.productId == productId)
+                    if(thisItem){
+                        sendResponse(res, false, null, "Item already in cart !", 400);
+                        return
+                    }
 
-                User.findOne({ _id: userId })
-                    .then(user => {
-                        if (user) {
+                }
+
+                Product.findOne({ _id: productId })
+                    .then(product => {
+                        if (product) {
+
+                            const newCartItem = new CartItem({
+                                productId,
+                                userId,
+                                quantity,
+                            })
+
                             user.cart.push(newCartItem);
                             user.save()
                                 .then(() => sendResponse(res, true, newCartItem, "Item added to cart !", 200))
                                 .catch(err => sendResponse(res, false, null, "Item not added to cart !", 400));
+
+
                         } else {
-                            sendResponse(res, false, null, "User not found !", 400);
+                            sendResponse(res, false, null, "Product not found !", 400);
                         }
                     })
-                    .catch(err => sendResponse(res, false, err, "User not found !", 400));
+                    .catch(err => sendResponse(res, false, err, "Product not found !", 400));
             } else {
-                sendResponse(res, false, null, "Product not found !", 400);
+                sendResponse(res, false, null, "User not found !", 400);
             }
+
         })
-        .catch(err => sendResponse(res, false, err, "Product not found !", 400));
+        .catch(err => sendResponse(res, false, err, "User not found !", 400));
+
+
+
 })
 
 
@@ -95,7 +111,7 @@ cartRouter.route("/updateQuantity").put((req, res) => {
                 console.log(user.cart);
                 if (thisItem) {
                     thisItem.quantity = quantity;
-                   
+
                     let temp = user.cart
                     let thisIndex = temp.findIndex(item => item.productId == cartItemId);
                     temp[thisIndex] = thisItem;
@@ -103,7 +119,8 @@ cartRouter.route("/updateQuantity").put((req, res) => {
                     user.save()
                         .then(() => {
                             console.log(user);
-                            sendResponse(res, true, thisItem, "Cart item updated !", 200)})
+                            sendResponse(res, true, thisItem, "Cart item updated !", 200)
+                        })
                         .catch(err => sendResponse(res, false, err, "Cart item not updated !", 400));
                 } else {
                     sendResponse(res, false, null, "Cart item not found !", 400);
